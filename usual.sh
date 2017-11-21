b041bad4fbcbfc15aed08a4e7cd7d0364124632c
Michael Collins <spod@users.noreply.github.com>
2017-11-20 23:01:19 -0800

    Update usual.sh

--- usual.sh
+++ usual.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # set up some crap which I do every time
-#touch ~/.hushlogin
+set -x
+set -e
+touch ~/.hushlogin
 (
 cat <<'EOF'
 set expandtab
@@ -20,7 +22,7 @@ bind k select-pane -U
 bind l select-pane -R
 EOF
 ) > ~/.tmux.conf
-mkdir ~/.ssh
+[ ! -d ~/.ssh ] && mkdir ~/.ssh
 (
 cat <<'EOF'
 ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCUJ4zBIqdnCm65DyzVY6gdEciky27+DmbZzPTNYgjtD3vJlYjSIRWwI9MMLdmti1hVzXJG1z8hqzuBDYG+nkZYpaJwQh7ElMxIAr6RXSXAxJCZRuUxhq+R9mbHzeA2au5WKHLEhBb6Nn4HWCgLLvoAsy2/rA5+CYnI1smE6Skie8f1K6FfnF5tAd+oexegv1JTGHsj4uWoRhbnyIKBS8tsMcOa8smx8RibFK5fC3/x5vJfdxIjwh9YH+LrE0DCQLM85IzW9wxQqnYEX3/HOhPNuvGAXdccmrHh4j7Iz26lqHu/ilfgEW4bh9VIoRiy34Z+/8IW4UgPMWqx/tUfQa5v michael@tabernac.nologin.net
