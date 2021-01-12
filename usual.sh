dc5b490d8d07d6acb29614c1d36b406d457b67c1
Michael Collins <michael.collins@gmail.com>
2021-01-11 21:30:24 -0800

    idempotentish

--- usual.sh
+++ usual.sh
@@ -2,8 +2,10 @@
 # set up some crap which I do every time
 set -x
 set -e
-touch ~/.hushlogin
-(
+
+[ ! -f ~/.hushlogin ] && touch ~/.hushlogin
+
+[ ! -f ~/.vimrc ] && (
 cat <<'EOF'
 set expandtab
 set ts=4
@@ -12,9 +14,10 @@ set nu
 syntax on
 EOF
 ) > ~/.vimrc
-(
+
+[ ! -f ~/.tmux.conf ] && (
 cat <<'EOF'
-set-option -g prefix C-a
+# set-option -g prefix C-a # make tmux use ctrl-a instead of ctrl-b
 setw -g mode-keys vi
 bind h select-pane -L
 bind j select-pane -D
@@ -22,40 +25,39 @@ bind k select-pane -U
 bind l select-pane -R
 EOF
 ) > ~/.tmux.conf
+
 [ ! -d ~/.ssh ] && mkdir ~/.ssh
-(
-cat <<'EOF'
-ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCUJ4zBIqdnCm65DyzVY6gdEciky27+DmbZzPTNYgjtD3vJlYjSIRWwI9MMLdmti1hVzXJG1z8hqzuBDYG+nkZYpaJwQh7ElMxIAr6RXSXAxJCZRuUxhq+R9mbHzeA2au5WKHLEhBb6Nn4HWCgLLvoAsy2/rA5+CYnI1smE6Skie8f1K6FfnF5tAd+oexegv1JTGHsj4uWoRhbnyIKBS8tsMcOa8smx8RibFK5fC3/x5vJfdxIjwh9YH+LrE0DCQLM85IzW9wxQqnYEX3/HOhPNuvGAXdccmrHh4j7Iz26lqHu/ilfgEW4bh9VIoRiy34Z+/8IW4UgPMWqx/tUfQa5v michael@tabernac.nologin.net
-EOF
-) > ~/.ssh/authorized_keys
-chmod go-rwx ~/.ssh/authorized_keys
+[ -f ~/.ssh/authorized_keys ] && touch ~/.ssh/authorized_keys && \
+chmod go-rwx ~/.ssh/authorized_keys && \
 chmod go-rwx ~/.ssh/
-(
+
+grep ixon ~/.bashrc || (
 cat <<'EOF'
-export TERM=xterm
+#export TERM=xterm
 # disable ctrl s/q
 stty -ixon
 EOF
 ) >> ~/.bashrc
-mkdir -p ~/bin
-(
+
+[ ! -d ~/bin ] && mkdir ~/bin
+
+[ ! -f ~/bin/git-git ] && (
 cat <<'EOF'
 #!/bin/bash
 git $*
 EOF
-) >> ~/bin/git-git
-chmod +x ~/bin/git-git
-(
+) >> ~/bin/git-git && chmod +x ~/bin/git-git
+
+[ ! -f ~/bin/git-what-branch ] && (
 cat <<'EOF'
 #! /bin/bash
 git rev-parse --abbrev-ref HEAD
 EOF
-) >> ~/bin/git-what-branch 
-chmod +x ~/bin/git-what-branch
-(
+) >> ~/bin/git-what-branch  && chmod +x ~/bin/git-what-branch
+
+[ ! -f ~/bin/git-branch-by-date ] && (
 cat <<'EOF'
 #! /bin/bash
 git for-each-ref --sort='-committerdate' --format='%(refname)%09%(committerdate)' refs/heads | sed -e 's-refs/heads/--'
 EOF
-) >> ~/bin/git-branch-by-date
-chmod +x ~/bin/git-branch-by-date
+) >> ~/bin/git-branch-by-date && chmod +x ~/bin/git-branch-by-date
